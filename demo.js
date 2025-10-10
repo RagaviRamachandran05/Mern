78f5dc81c5c67b522c8c23d4123dd3643177c54a
RagaviRamachandran05 <rragavi054@gmail.com>
2025-10-10 16:07:10 +0530

    js

---
+++ demo.js
@@ -0,0 +1,16 @@
+let a={age:33};
+let b=a;
+a.age=22;
+console.log(b.age);
+
+function mul(a,b){
+    return a+b;
+}
+console.log(mul(11,22));
+
+
+const sum=(a,b)=>{
+    console.log(a+b);
+}
+sum(20,30);
+
