d86337f8031fce621663b96060b43b12855d4603
RagaviRamachandran05 <rragavi054@gmail.com>
2026-01-22 13:12:50 +0530

    task

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,15 +1,82 @@
-const Task=require('../models/task')
+const Task = require('../models/task')
 
-exports.createTask=async(req,res)=>{
-  try{
-    const task=await Task.create({
-        title:req.body.title,
-        description:req.body.description,
-        status:req.body.status||'pending',
-        user:req.userId
+exports.createTask = async (req, res) => {
+  try {
+    const task = await Task.create({
+      title: req.body.title,
+      description: req.body.description,
+      status: req.body.status || "pending",
+      user: req.user.id 
     })
-    res.status(201).json({msg:"task created Successfully",task})
+    res.status(201).json({ msg: "Task created successfully", task })
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+exports.getTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({ user: req.user.id })
+    res.json(tasks)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+exports.getTask = async (req, res) => {
+  try {
+    const task = await Task.findOne({ user: req.user.id })
+    if (!task) return res.status(404).json({ msg: "Task not found" })
+    res.json(task)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+exports.getTasksById = async (req, res) => {
+  try {
+    const task = await Task.findOne({
+      _id: req.params.id,
+      user: req.user.id
+    })
+    if (!task) return res.status(404).json({ msg: "Task not found" })
+    res.json(task)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+}
+
+exports.updateTask=async(req,res)=>{
+  try{
+    const task=await Task.findOneAndUpdate({
+    _id:req.params.id,
+    user:req.user.id,
+   },
+   {
+    title:req.body.title,
+    description:req.body.description,
+    status:req.body.status
+   },{new:true})
+   if(!task){
+    res.json({msg:"No task Exist"})
+   }
+   res.json(task);
   }catch(error){
     res.status(300).send(error)
+  }
+}
+
+exports.deleteTask=async(req,res)=>{
+  try{
+    const task=await Task.findOneAndDelete({
+      _id:req.params.id,
+      user:req.user.id
+    })
+    if(!task){
+      res.json({msg:"No task Exist"})
+    }
+    res.json({msg:"Task deleted successfully", task})
+  }catch(error){
+    res.send(error)
   }
 }
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,8 +1,28 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
 
-const { createTask } = require('../controllers/taskController')
+const {
+  createTask,
+  getTasks,
+  getTask,
+  getTasksById,
+  updateTask,
+  deleteTask
+} = require("../controllers/taskController")
 
-router.post('/newTask', createTask)
+const { protect } = require('../middleware/authmiddleware')
+
+router.post('/newTask', protect, createTask)
+
+router.get('/getTasks', protect, getTasks)
+
+router.get('/getTasks/:id', protect, getTasksById)
+
+router.get('/getTask', protect, getTask)
+
+router.put('/updateTask/:id', protect,updateTask)
+
+router.delete('/deleteTask/:id', protect,deleteTask)
+
 
 module.exports = router
